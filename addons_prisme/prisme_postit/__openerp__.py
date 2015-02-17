59ada76ea9eaebf1202c7f680a86e454e6c13f93
loicbaechler <loic.baechler@prisme.ch>
2015-02-17 09:55:10 +0100

    Migration - Prisme postit

---
+++ addons_prisme/prisme_postit/__openerp__.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'prisme_postit',
+    'version': '1.0',
+    'category': 'Tools',
+    'description': """
+This module allows users to create their own postit inside OpenERP
+=================================================================
+
+
+""",
+    'author': 'Prisme Solutions informatique SA',
+    'website': 'http://openerp.com',
+    'summary': 'Managing tasks and reminders',
+    'sequence': 9,
+    'depends': [
+        'mail',
+    ],
+    'data': [
+      #  'security/note_security.xml',
+      #  'security/ir.rule.xml',
+      #  'security/ir.model.access.csv',
+        'postit_view.xml',
+        'postit_workflow.xml',
+
+    ],
+    'demo': [
+
+    ],
+    'test': [
+    ],
+    'init_xml': [
+    'init_scheduler.xml',
+
+    ],
+    'update_xml': [
+    'security/ir.model.access.csv',
+    ],
+    'css': [
+      #  'static/src/css/note.css',
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ addons_prisme/prisme_postit/postit_day.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models
+
+class prisme_postit_day(models.Model):
+    _name = 'prisme.postit.day'
+    _description = "Postit Day"
+
+    name = fields.Char(string="Day name", required=True, translate=True)
+    nbr = fields.Integer('Day number', required=True)
+
+prisme_postit_day()
