82934a74950f5c75bfdfe9e5ff921fc4391f5fdf
davidp <david.progin@prisme.ch>
2020-04-24 11:57:18 +0200

    prisme_client_accounting_line : added v12 sources

---
+++ prisme_client_accounting_line/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Prisme Client Accounting Line",
+    "version": "2019-12-03 14:00",
+    "author": "Prisme Solutions Informatique SA",
+    "category": "Generic Modules/Accounting",
+    "website": "https://www.prisme.ch",
+    "summary": "List of client accounting lines",
+    "depends": [
+        'account',
+    ],
+    "demo": [],
+    "data": [
+        "view/view_accounting_line_client.xml",
+        "view/view_invoice_line_form.xml",
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+    "active": False,
+    "sequence":150,
+}
